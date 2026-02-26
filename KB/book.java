022ae8d8f912f8a2b5099d377d17c213ccc34134
KeyDoge <keylendb@gmail.com>
2026-02-26 16:52:11 -0600

    Add files via upload

---
+++ KB/book.java
@@ -0,0 +1,8 @@
+public class book {
+    //Should have attributes: title, authour, ISBN, and isAvailable
+    public String title;
+    public String author;
+    public String ISBN;
+    public boolean isAvailable;
+    
+}
---
+++ KB/student.java
@@ -0,0 +1,8 @@
+public class student {
+    public String name;
+    public int studentID;
+    //borrowedBooks (list of books that the student has borrowed)
+    public book[] borrowedBooks;
+
+
+}
