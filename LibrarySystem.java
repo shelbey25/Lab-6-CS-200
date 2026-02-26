fbcd96a148ee1995046588fb2941a49d168ecfbd
shelbey25 <sbyousey@crimson.ua.edu>
2026-02-26 17:05:52 -0600

    Track books in system

--- LibrarySystem.java
+++ LibrarySystem.java
@@ -1,4 +1,8 @@
+import java.util.Vector;
+
 public class LibrarySystem{
+
+    private Vector<String> borrowedBooks;
 
     public static void borrowBook(Student student, Book book) {
         //need logic here
